9d7ebec03c898f8a65f2a875087787883711519f
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-09-06 10:55:05 +0430

    practice state in react

--- state/src/Components/NewComp.js
+++ state/src/Components/NewComp.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import bell from '../images/bell.png';
+import emptybell from '../images/bell_empty.png';
 
 export class NewComp extends Component {
     constructor(props) {
@@ -6,17 +8,33 @@ export class NewComp extends Component {
     
       this.state = {
          message: "Subscribe React Learning",
-         sub: "Subscribed"
+         sub: "Subscribe",
+         imageURL: emptybell
       }
     }
     ButtonChange = () => {
-        this.setState({sub: 'Unsubscribe'})
+        this.setState({
+            sub: 'subscribed',
+            message: 'Hit the bell icon never miss an update'
+        })
+    }
+    ImageChange = () => {
+        this.setState(
+            {
+                message: 'Enjoy From Learning React!',
+                imageURL: bell
+            }
+        )
     }
     render() {
         return (
             <div>
                 <p>{this.state.message}</p>
                 <button onClick={this.ButtonChange}>{this.state.sub}</button>
+                <br />
+                <br />
+                {this.state.sub === 'subscribed' ? 
+                    <img src={this.state.imageURL} style={{ width: '32px', height: '32px'}} alt="" onClick={this.ImageChange} /> : null }
             </div>
         )
     }
