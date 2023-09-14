d37bdd2afdb5b58ea457f019d8ff4541d03acbea
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-09-14 16:23:02 +0430

    make the layout component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,12 @@
 import './Style.scss';
-import { Router } from 'react-router-dom';
+import { Route, Router } from 'react-router-dom';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <div>
       <Router>
-        
+        <Route path="/" element={<Layout />} />
       </Router>
     </div>
   );
