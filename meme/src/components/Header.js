422f1da9bbc08ec96ca16a295fe542d67e2f2aed
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-09-17 10:44:44 +0430

    [IMP] header component

---
+++ meme/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeaderImage from './../images/img.png';
+import './header.css';
+
+export default function Header() {
+  return (
+    <header className='header'>
+        <img className='header--image' src={HeaderImage} />
+        <h2 className='header--title'>Meme Generator</h2>
+        <h4 className='header--project'>JavaScript-Project</h4>
+    </header>
+  )
+}
