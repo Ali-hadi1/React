c9a1da4c95563e18e274ea004b18859e63c9b107
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-09-25 11:27:51 +0430

    IMP boxes array data for challenge and Box component

--- box-challenge/src/App.js
+++ box-challenge/src/App.js
@@ -1,11 +1,18 @@
+import React from "react"
 import './App.css';
+import boxes from "./boxes"
+import Box from './components/Box'
 
-function App() {
-  return (
-    <div className="App">
-      ASDF
-    </div>
-  );
+export default function App() {
+    const [squares, setSquares] = React.useState(boxes)
+    
+    const squareElements = squares.map(square => (
+        <Box key={square.id} on={square.on} />
+    ))
+    
+    return (
+        <main>
+            {squareElements}
+        </main>
+    )
 }
-
-export default App;
