dfc6cad8715ddda2867e10dce42b6b01ea2fe613
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-10-04 13:53:58 +0430

    added firebase configration file

---
+++ notes/src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore"
+const firebaseConfig = {
+  apiKey: "AIzaSyCP520U1fZe1yvLaFOewDmsM8DvFuhEimg",
+  authDomain: "react-notes-af3c1.firebaseapp.com",
+  projectId: "react-notes-af3c1",
+  storageBucket: "react-notes-af3c1.appspot.com",
+  messagingSenderId: "472993933869",
+  appId: "1:472993933869:web:537f7b192ac42f667aa6a4"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+const notesCollection = collection(db, "notes")
