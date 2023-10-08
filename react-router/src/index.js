04b0525dc957963ae029c605f9bad62dcc978ec1
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-10-08 14:15:35 +0430

    IMP two route in react

--- react-router/src/index.js
+++ react-router/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import About from './components/About';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App/>}/>
+      <Route path='/about' element={<About />} />
     </Routes>
   </BrowserRouter>
 );
