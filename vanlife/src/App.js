8dd87f44c11549f18422343073760d5e86bcec27
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-10-08 15:50:25 +0430

    IMP Home and About page

--- vanlife/src/App.js
+++ vanlife/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 
 
 function App() {
   return (
     <Router>
+      <header>
+        <Link to="/">#VANLIFE</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
