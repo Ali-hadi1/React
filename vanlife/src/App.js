0612fdc33f9ffc6c7993d64d28cfcb7a962cdd40
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-10-09 10:19:00 +0430

    [IMP] vans list page

--- vanlife/src/App.js
+++ vanlife/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Vans from './components/pages/Vans';
 
 
 function App() {
@@ -10,12 +11,14 @@ function App() {
       <header>
         <Link to="/">#VANLIFE</Link>
         <nav>
+          <Link to="/vans">Vans</Link>
           <Link to="/about">About</Link>
         </nav>
       </header>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
+        <Route path='/vans' element={<Vans />} />
       </Routes>
     </Router>
   );
