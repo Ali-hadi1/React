8c1b08750e647d4d23d638ef6655c14b1204820b
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-12-05 09:51:31 +0430

    [IMP] vans list

--- vanlife/src/server.js
+++ vanlife/src/server.js
@@ -14,7 +14,7 @@ createServer({
             description: `The Modest Explorer is a van designed to get you out of the house and into nature.
                 This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette.
                 The idea is that you can pack up your home and escape for a weekend or even longer!`, 
-            imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", type: "simple" }
+            imageUrl: "https://assets.scrimba.com/advanced-react/react-router/modest-explorer.png", type: "simple", hostId: "123" }
         )
         server.create("van", { 
             id: "2", 
@@ -24,7 +24,7 @@ createServer({
                 Beach Bum is a van inspired by surfers and travelers. 
                 It was created to be a portable home away from home, but with some cool features in it you won't find in an ordinary camper.`, 
             imageUrl: "https://assets.scrimba.com/advanced-react/react-router/beach-bum.png", 
-            type: "rugged" }
+            type: "rugged", hostId:"123" }
         )
         server.create("van", { 
             id: "3", 
@@ -35,7 +35,7 @@ createServer({
                 The inside is comfortable and cozy, with plenty of space to stretch out in. There's a small kitchen, so you can cook if you need to. 
                 You'll feel like home as soon as you step out of it.`, 
             imageUrl: "https://assets.scrimba.com/advanced-react/react-router/reliable-red.png", 
-            type: "luxury" })
+            type: "luxury", hostId: "456" })
         server.create("van", { 
             id: "4", 
             name: "Dreamfinder", 
@@ -46,7 +46,7 @@ createServer({
                 The floor is a beautiful glass-reinforced plastic (GRP) which is easy to clean and very hard wearing. 
                 A large rear window and large side windows make it really light inside and keep it well ventilated.`, 
             imageUrl: "https://assets.scrimba.com/advanced-react/react-router/dreamfinder.png", 
-            type: "simple" }
+            type: "simple", hostId: "789" }
         )
         server.create("van", { 
             id: "5", 
@@ -56,7 +56,7 @@ createServer({
                 The Cruiser is a van for those who love to travel in comfort and luxury. 
                 With its many windows, spacious interior and ample storage space, the Cruiser offers a beautiful view wherever you go.`, 
             imageUrl: "https://assets.scrimba.com/advanced-react/react-router/the-cruiser.png", 
-            type: "luxury" }
+            type: "luxury", hostId: "789" }
         )
         server.create("van", { 
             id: "6", 
@@ -67,7 +67,7 @@ createServer({
                 The Green Wonder is a sustainable vehicle that's perfect for people who are looking for a stylish, 
                 eco-friendly mode of transport that can go anywhere.`, 
             imageUrl: "https://assets.scrimba.com/advanced-react/react-router/green-wonder.png", 
-            type: "rugged" }
+            type: "rugged", hostId: "123" }
         )
     },
 
@@ -81,6 +81,17 @@ createServer({
         this.get("/vans/:id", (schema, request) => {
             const id = request.params.id
             return schema.vans.find(id)
+        })
+
+        this.get("/host/vans", (schema, request) => {
+            // Hard-code the hostId for now
+            return schema.vans.where({ hostId: "123" })
+        })
+
+        this.get("/host/vans/:id", (schema, request) => {
+            // Hard-code the hostId for now
+            const id = request.params.id
+            return schema.vans.where({ id, hostId: "123" })
         })
     }
 })
