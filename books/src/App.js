05863b0c811dd1cbd73a2ffa186c02a2a53df1f8
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-12-05 08:43:52 +0430

    [IMP] Book create component

--- books/src/App.js
+++ books/src/App.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import './index.css';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  const createBook = (title) => {
+    console.log('>>>>>>>>>>>>>>>>>>>>>', title);
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <BookCreate onCreate={createBook}/>
+    </div>
   );
 }
 
