7eb8579174a45aa4eab67eef02ebd1fe2e479dc8
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-12-10 21:31:11 +0430

    added random images for books

--- books/src/components/BookShow.js
+++ books/src/components/BookShow.js
@@ -17,6 +17,7 @@ export default function BookShow({ book, onDelete, onEdit }) {
   }
   return (
     <div className='book-show'>
+      <img alt='Books' src={`https://picsum.photos/seed/${book.id}/300/200`}/>
       <div>{content}</div>
       <div className='actions'>
         <button className='edit' onClick={handleEditClick}>Edit</button>
