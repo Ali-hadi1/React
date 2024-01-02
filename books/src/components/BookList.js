1da9e517eda128ed034663d3816d8b54b035b2e4
Ali-hadi1 <hadiadelzada2011@gmail.com>
2024-01-02 14:50:44 +0430

    practice sharing data with context in react

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react';
 import BookShow from './BookShow';
+import BooksContext from '../context/book';
 
 export default function BookList({ books, onDelete, onEdit}) {
+	const { count, incrementCount } = useContext(BooksContext);
 	const renderedBooks = books.map((book) => {
 		return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book}/>
 	})
 	return (
-		<div className='book-list'>{renderedBooks}</div>
+		<div className='book-list'>
+			{count}
+			<button onClick={incrementCount}>Click</button>
+			{renderedBooks}
+		 </div>
 	)
 }
