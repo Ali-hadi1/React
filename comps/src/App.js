d83e30e83e8905f15145c10695cbba20696314c6
Ali-hadi1 <hadiadelzada2011@gmail.com>
2024-01-08 22:18:53 +0430

    IMP close button to the component

--- comps/src/App.js
+++ comps/src/App.js
@@ -9,6 +9,7 @@ const message = [
 
 function App() {
 	const [step, setStep] = useState(1);
+	const [isOpen, setIsOpen] = useState(true)
 
 	function handlePrevious() {
 		if (step > 1) setStep(step - 1)
@@ -19,19 +20,27 @@ function App() {
 	}
 
 	return (
-		<div className='flex flex-col w-4/12 m-auto mt-16 rounded-md px-16 py-3 bg-slate-100'>
-			<div className='flex justify-between'>
-				<div className={`flex justify-center items-center w-6 h-6 rounded-full ${step >= 1 ? ' bg-indigo-600 text-white' : 'bg-slate-300'}`}>1</div>
-				<div className={`flex justify-center items-center w-6 h-6 rounded-full ${step >= 2 ? ' bg-indigo-600 text-white' : 'bg-slate-300'}`}>2</div>
-				<div className={`flex justify-center items-center w-6 h-6 rounded-full ${step >= 3 ? ' bg-indigo-600 text-white' : 'bg-slate-300'}`}>3</div>
+		<div>
+			<div className='flex flex-col w-4/12 m-auto mt-16'>
+				<button className='self-end' onClick={() => setIsOpen(!isOpen)}>&times;</button>
 			</div>
-			<div className='py-8 self-center'>
-				<p className='font-medium'>Step {step}: {message[step -1]}</p>
-			</div>
-			<div className='flex justify-between'>
-				<button className='px-2 py-0.5 rounded-2xl bg-indigo-600 text-white' onClick={handlePrevious}>Previous</button>
-				<button className='px-2 py-0.5 rounded-2xl bg-indigo-600 text-white' onClick={handleNext}>Next</button>
-			</div>
+			{
+				isOpen &&
+				(<div className='flex flex-col w-4/12 m-auto rounded-md px-16 py-3 bg-slate-100'>
+					<div className='flex justify-between'>
+						<div className={`flex justify-center items-center w-6 h-6 rounded-full ${step >= 1 ? ' bg-indigo-600 text-white' : 'bg-slate-300'}`}>1</div>
+						<div className={`flex justify-center items-center w-6 h-6 rounded-full ${step >= 2 ? ' bg-indigo-600 text-white' : 'bg-slate-300'}`}>2</div>
+						<div className={`flex justify-center items-center w-6 h-6 rounded-full ${step >= 3 ? ' bg-indigo-600 text-white' : 'bg-slate-300'}`}>3</div>
+					</div>
+					<div className='py-8 self-center'>
+						<p className='font-medium'>Step {step}: {message[step -1]}</p>
+					</div>
+					<div className='flex justify-between'>
+						<button className='px-2 py-0.5 rounded-2xl bg-indigo-600 text-white' onClick={handlePrevious}>Previous</button>
+						<button className='px-2 py-0.5 rounded-2xl bg-indigo-600 text-white' onClick={handleNext}>Next</button>
+					</div>
+				</div>)
+			}
 		</div>
 	);
 }
