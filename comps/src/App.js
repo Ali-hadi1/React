61f0575125cad2984ecbd8c573e4e765ebba75a1
Ali-hadi1 <hadiadelzada2011@gmail.com>
2024-01-08 09:29:02 +0430

    IMP accordion component

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,9 +1,25 @@
+import Accordion from './components/Accordion';
 import './css/index.css';
 
 function App() {
+  const items = [
+    {
+      label: 'Can I user React on a project?',
+      content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. '
+    },
+    {
+      label: 'Can I user Javascript on a project?',
+      content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. '
+    },
+    {
+      label: 'Can I user Vue JS on a project?',
+      content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. '
+    },
+  ]
   return (
     <div>
-      App
+      <Accordion />
+      
     </div>
   );
 }
