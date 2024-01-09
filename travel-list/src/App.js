e0838e4077ab81e4cd633d81017179d1abb71327
Ali-hadi1 <hadiadelzada2011@gmail.com>
2024-01-09 11:44:04 +0430

    IMP the travel app in single file

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,10 +1,88 @@
-import './css/index.css';
+const initialItems = [
+  {
+    id: 1,
+    description: "Passports",
+    quantity: 2,
+    packed: false
+  },
+  {
+    id: 2,
+    description: "Socks",
+    quantity: 12,
+    packed: true
+  },
+  {
+    id: 3,
+    description: "Charger",
+    quantity: 1,
+    packed: true
+  },
+]
 
-function App() {
+
+export default function App() {
   return (
-    <div>App</div>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
   )
 }
 
+function Logo() {
+  return <h1>🏝️ Far Away 🧳</h1>
+}
 
-export default App;
+function Form() {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+  }
+
+  return <form className="add-form" onSubmit={handleSubmit}>
+    <h3>What do you need for your 😍 trip?</h3>
+    <select>
+      {Array.from({length: 20}, (_,i) => i+1).map
+      ((num) => (
+        <option value={num} key={num}>
+          {num}
+        </option>
+      ))}
+    </select>
+    <input type="text" placeholder="Item..."/>
+    <button>Add</button>
+  </form>
+}
+
+function PackingList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) =>( <Item key={item.id } item={item} />))}
+      </ul>
+    </div>
+  )
+}
+
+function Item({item}) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration:"line-through"} : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌ </button>
+    </li>
+  )
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on you list, and you already packed X (X%)</em>
+    </footer>
+  )
+}
+
