8714fa81b99ba9d85819128c45b6a1525171f38a
Ali-hadi1 <hadiadelzada2011@gmail.com>
2024-01-10 11:01:58 +0430

    IMP the form component design

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   {
     id: 1,
@@ -36,15 +38,23 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("")
+  const [quantity, setQuantity] = useState(1)
 
   function handleSubmit(e) {
     e.preventDefault();
+    if(!description) return;
 
+    const  newItem = {description, quantity, packed: false, id: Date.now()}
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1)
   }
 
   return <form className="add-form" onSubmit={handleSubmit}>
     <h3>What do you need for your 😍 trip?</h3>
-    <select>
+    <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
       {Array.from({length: 20}, (_,i) => i+1).map
       ((num) => (
         <option value={num} key={num}>
@@ -52,7 +62,7 @@ function Form() {
         </option>
       ))}
     </select>
-    <input type="text" placeholder="Item..."/>
+    <input type="text" placeholder="Item..." value={description} onChange={(e) => setDescription(e.target.value)}/>
     <button>Add</button>
   </form>
 }
