83446ee4985703ddda1ddb3ff87e172dcd98c657
Ali-hadi1 <hadiadelzada2011@gmail.com>
2024-02-13 13:21:10 +0430

    IMP split bill form

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -24,7 +24,10 @@ export default  function App() {
     <div className="app">
       <div className="sidebar">
         <FriendsList />
+        <FormAddFriend/>
+        <Button>Add Friend</Button> 
       </div>
+      <FormSplitBill/>
     </div>
   );
 }
@@ -63,6 +66,52 @@ function Friend({friend}) {
       )}
       <button className="button">Select</button>
     </li>
+  )
+}
+
+function Button({children}) {
+  return (
+    <button className="button">{children}</button>
+  )
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label>👨‍❤️‍👨 Friend Name</label>
+      <input type="text"/>
+
+      <label>🖼 Image URL</label>
+      <input type="text"/>
+
+      <Button>Add</Button>
+    </form>
+  )
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text"/>
+
+      <label>👱 Your Expense</label>
+      <input type="text"/>
+
+      <label>👩‍❤️‍👩  X's expense</label>
+      <input type="text" disabled/>
+
+      <label>🤑 Who is paying the bill</label>
+      <select>
+        <option value='user'>You</option>
+        <option value='friend'>X</option>
+      </select>
+
+      <Button>Split bill</Button>
+
+    </form>
   )
 }
 
