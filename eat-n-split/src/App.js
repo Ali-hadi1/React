b028cdf8e26ffa229173a6c16c3331de31c9591f
Ali-hadi1 <hadiadelzada2011@gmail.com>
2024-02-13 13:42:08 +0430

    displaying the new friends from conditionaly

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+
+
 const initialFriends = [
   {
     id: 118836,
@@ -20,12 +23,17 @@ const initialFriends = [
 ];
 
 export default  function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show)
+  }
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend/>
-        <Button>Add Friend</Button> 
+        {showAddFriend && <FormAddFriend/>}
+        <Button onClick={handleShowAddFriend}>{showAddFriend ? "Close" : "Add Friend"}</Button> 
       </div>
       <FormSplitBill/>
     </div>
@@ -69,9 +77,9 @@ function Friend({friend}) {
   )
 }
 
-function Button({children}) {
+function Button({onClick, children}) {
   return (
-    <button className="button">{children}</button>
+    <button className="button" onClick={onClick}>{children}</button>
   )
 }
 
